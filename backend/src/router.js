370dde3c70a4f3e5cc6a816df6089ff6c6231dfc
Camfabjeh <camille.fabry@yahoo.com>
2023-05-29 10:16:50 +0200

    création des fichiers articlesControllers et ArticlesManager + création des routes dans le router + création de la DB

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,12 +2,20 @@ const express = require("express");
 
 const router = express.Router();
 
-const itemControllers = require("./controllers/itemControllers");
+// const itemControllers = require("./controllers/itemControllers");
 
-router.get("/items", itemControllers.browse);
-router.get("/items/:id", itemControllers.read);
-router.put("/items/:id", itemControllers.edit);
-router.post("/items", itemControllers.add);
-router.delete("/items/:id", itemControllers.destroy);
+// router.get("/items", itemControllers.browse);
+// router.get("/items/:id", itemControllers.read);
+// router.put("/items/:id", itemControllers.edit);
+// router.post("/items", itemControllers.add);
+// router.delete("/items/:id", itemControllers.destroy);
+
+const articlesControllers = require("./controllers/articlesControllers");
+
+router.get("/items", articlesControllers.browse);
+router.get("/items/:id", articlesControllers.read);
+router.put("/items/:id", articlesControllers.edit);
+router.post("/items", articlesControllers.add);
+router.delete("/items/:id", articlesControllers.destroy);
 
 module.exports = router;
