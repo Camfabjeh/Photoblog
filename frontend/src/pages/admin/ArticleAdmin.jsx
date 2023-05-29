9deb7223a64db7f6694b770feda3c7e894c8a947
Camfabjeh <camille.fabry@yahoo.com>
2023-05-29 15:22:32 +0200

    récupération des data et contrôle des champs via stat

--- frontend/src/pages/admin/ArticleAdmin.jsx
+++ frontend/src/pages/admin/ArticleAdmin.jsx
@@ -1,28 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ArticleAdmin() {
+  const [article, setArticle] = useState({
+    title: "",
+    subtitle: "",
+    resume: "",
+    src: "",
+    alt: "",
+    tags: [],
+  });
+
+  const handleArticle = (name, value) => {
+    setArticle({ ...article, [name]: value });
+  };
   return (
     <div className="container">
       <form className="row">
         <label htmlFor="">
           Titre
-          <input type="text" required minLength={5} maxLength={255} />
+          <input
+            type="text"
+            required
+            minLength={5}
+            maxLength={255}
+            name="title"
+            onChange={(e) => handleArticle(e.target.name, e.target.value)}
+            value={article.title}
+          />
         </label>
         <label htmlFor="">
           Sous-titre
-          <input type="text" required minLength={5} maxLength={255} />
+          <input
+            type="text"
+            required
+            minLength={5}
+            maxLength={255}
+            name="subtitle"
+            onChange={(e) => handleArticle(e.target.name, e.target.value)}
+            value={article.subtitle}
+          />
         </label>
         <label htmlFor="">
           Résumé
-          <textarea required minLength={5} />
+          <textarea
+            required
+            minLength={5}
+            name="resume"
+            onChange={(e) => handleArticle(e.target.name, e.target.value)}
+            value={article.resume}
+          />
         </label>
         <label htmlFor="">
           Source image
-          <input type="text" required minLength={5} />
+          <input
+            type="text"
+            required
+            minLength={5}
+            name="src"
+            onChange={(e) => handleArticle(e.target.name, e.target.value)}
+            value={article.src}
+          />
         </label>
         <label htmlFor="">
           Texte alternatif
-          <input type="text" required minLength={1} maxLength={255} />
+          <input
+            type="text"
+            required
+            minLength={1}
+            maxLength={255}
+            name="alt"
+            onChange={(e) => handleArticle(e.target.name, e.target.value)}
+            value={article.alt}
+          />
         </label>
         <label htmlFor="">
           Liste de tags
